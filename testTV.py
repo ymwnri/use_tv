05f7d99a8973e426e4cca174c0e92497fc538a28
Aidan Tiu <tiuprof77@gmail.com>
2024-05-19 14:02:56 +0800

    Added method tests

--- testTV.py
+++ testTV.py
@@ -16,9 +16,32 @@ def main():
     tv2.set_channel(3)
     tv2.set_volume(2)
 
+    # Display the channel and volume for both tv1 and tv2
     print(f"tv1's channel is {tv1.get_channel()} and volume is {tv1.get_volume()}")
     print(f"tv2's channel is {tv2.get_channel()} and volume is {tv2.get_volume()}")
 
+    # Adjust the channel and volume for both tv1 and tv2 
+    tv1.channel_up()
+    tv1.volume_up()
+    print(f"tv1's channel is {tv1.get_channel()} and volume is {tv1.get_volume()}")
+
+    # Adjust the channel and volume for both tv1 and tv2
+    tv2.channel_down()
+    tv2.volume_down()
+    print(f"tv2's channel is {tv2.get_channel()} and volume is {tv2.get_volume()}")
+
+    # Turn off tv1 and tv2
+    tv1.turn_off()
+    tv2.turn_off()
+    print(f"tv1 is {tv1.get_state()}")
+    print(f"tv2 is {tv2.get_state()}")
+
+    # Try to set the channel and volume for tv1 and tv2 when they are off
+    tv1.set_channel(50)
+    tv1.set_volume(5)
+    tv2.set_channel(5)
+    tv2.set_volume(4)
+    print(f"tv1's channel is {tv1.get_channel()} and volume is {tv1.get_volume()}")
 
 if __name__ == "__main__":
     main()
