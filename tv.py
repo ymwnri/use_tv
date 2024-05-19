374f6de42ede53d67629acebac5e0ce711753630
Aidan Tiu <tiuprof77@gmail.com>
2024-05-19 12:29:54 +0800

    Create on/off methods

--- tv.py
+++ tv.py
@@ -10,3 +10,11 @@ class TV:
         self.channel: int = 1
         self.volume: int = 1
         self.on: bool = False
+
+    # Turn on the TV
+    def turn_on(self) -> None:
+        self.on = True
+
+    # Turn off the TV
+    def turn_off(self) -> None:
+        self.on = False
