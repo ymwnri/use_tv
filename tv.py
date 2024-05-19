ab8defede8e30b11f50b97e672654c30393c7bc7
Aidan Tiu <tiuprof77@gmail.com>
2024-05-19 12:27:59 +0800

    Initiialize object

--- tv.py
+++ tv.py
@@ -2,3 +2,11 @@
 # Define methods: turn on/off, get channel, set channel
 # get volume, set volume, channel up, channel down
 # volume up, volume down
+
+class TV:
+
+    # Initialize state variables and their types
+    def __init__(self) -> None:
+        self.channel: int = 1
+        self.volume: int = 1
+        self.on: bool = False
