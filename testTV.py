a929638eae66acfd4d3060dcaecd9683f3aa998e
Aidan Tiu <tiuprof77@gmail.com>
2024-05-19 12:45:52 +0800

    Add pseudocode

---
+++ testTV.py
@@ -0,0 +1,4 @@
+# Import the TV class from the tv.py
+# Create 2 TV objects
+# Make the first TV object turn on and set the channel to 30 and volume to 3
+# Make the second TV object turn on and set the channel to 3 and volume to 2
